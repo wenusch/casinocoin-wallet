dd268fd2b2794a0bf3ecfafebafc1acd5157cbff
Nikhil Gupta <nikhil.gupta@issc.in>
2018-03-15 16:47:00 +0530

    Resolved paper wallet print issue

--- src/app/components/forms/paperwallet/paperwallet.component.ts
+++ src/app/components/forms/paperwallet/paperwallet.component.ts
@@ -10,6 +10,7 @@ import { SelectItem, MenuItem } from 'primeng/primeng';
 import { CSCUtil } from '../../../domain/csc-util';
 import * as keypairs from 'casinocoin-libjs-keypairs';
 import { QRCodeModule } from 'angular2-qrcode';
+import { Router } from '@angular/router';
 
 import { WindowRef } from './WindowRef';
 declare var casinocoin: any;
@@ -27,7 +28,12 @@ export class PaperwalletComponent implements OnInit {
     accounts: Array<LokiAccount> = [];
     keySet: any[] = [];
 
-    constructor(private winRef: WindowRef,private logger: LogService, private casinocoinService: CasinocoinService, private walletService: WalletService, private electronService: ElectronService) { 
+    constructor(private winRef: WindowRef,
+        private logger: LogService, 
+        private casinocoinService: CasinocoinService, 
+        private walletService: WalletService, 
+        private electronService: ElectronService,
+        private router: Router,) { 
         this.logger.debug("### INIT Paperwallet ###");
     }
 
@@ -36,9 +42,14 @@ export class PaperwalletComponent implements OnInit {
 
     //the method we are using currently
     print(): void {
+
+        const electron = require('electron');
+        const BrowserWindow = electron.remote.BrowserWindow;
+
         let printContents;
         printContents = this.winRef.nativeWindow.document.getElementById('printsection').innerHTML;
-        //this.winRef.nativeWindow.document.open();
+        
+        /*this.winRef.nativeWindow.document.open();
         this.winRef.nativeWindow.document.write(`
           <html>
             <head>
@@ -47,11 +58,59 @@ export class PaperwalletComponent implements OnInit {
               //........Customized style.......
               </style>
             </head>
-            <body align="center"><h1>Paper Wallet</h1>${printContents}</body>
+            <body align="center"><h1>Paper Wallet</h1>${printContents}
+                <button type="button" onclick="redirect()">GO Back</button>
+                <nav>
+                <a routerLink="/paperwallet" href="#"> Anchor </a>
+                </nav>
+                <nav>
+                <button routerLink="/paperwallet"> Anchor </button>
+                </nav>
+                <button routerLink="/paperwallet"> Anchor </button>
+                <script>
+                    function redirect() {
+                        console.log(document.location.href);
+                        window.location = document.location.href;
+                    }
+                </script>
+            </body>
           </html>`
         );
-        this.winRef.nativeWindow.print();
-        this.winRef.nativeWindow.document.close();
+        this.winRef.nativeWindow.print();    
+        this.winRef.nativeWindow.document.close();  
+        this.winRef.nativeWindow.location = __dirname + '/index.html#/home/paperwallet'; */
+
+        let win = new BrowserWindow({width: 800, height: 700,icon: __dirname + '/favicon.ico'})
+        win.on('closed', () => {
+          win = null;
+        })
+        const loadView = ({title,scriptUrl}) => {
+          return (`
+          <html>
+            <head>
+              <title>Print tab</title>
+              <style>
+              //........Customized style.......
+              </style>
+            </head>
+            <body align="center" onload="printFunction()">
+            <h1>Paper Wallet</h1>${printContents}
+            <script>
+            function printFunction() {
+                window.print();
+            }
+            </script>
+            </body>
+          </html>`)
+        }
+        
+        var file = 'data:text/html;charset=UTF-8,' + encodeURIComponent(loadView({
+          title: "PaperWallet",
+          scriptUrl: "./paperWallet.view.js"
+        }));
+        win.setMenu(null);
+        win.loadURL(file);
+        //win.webContents.print();
     }
 
     showCreateAddress(){
