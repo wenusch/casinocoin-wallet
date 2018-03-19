511309e199a2c8edfd4693f3737bc5d9838fc244
Nikhil Gupta <nikhil.gupta@issc.in>
2018-03-19 13:38:54 +0530

    Updated paperwallet

--- src/app/components/forms/paperwallet/paperwallet.component.ts
+++ src/app/components/forms/paperwallet/paperwallet.component.ts
@@ -13,8 +13,6 @@ import { QRCodeModule } from 'angular2-qrcode';
 import { Router } from '@angular/router';
 
 import { WindowRef } from './WindowRef';
-declare var casinocoin: any;
-declare var QRCode: any;
 
 @Component({
     selector: 'app-paperwallet',
@@ -27,6 +25,8 @@ export class PaperwalletComponent implements OnInit {
     checkBox: boolean = true;
     accounts: Array<LokiAccount> = [];
     keySet: any[] = [];
+    showInstructions: boolean = false;
+    address_context_menu: ElectronMenu;
 
     constructor(private winRef: WindowRef,
         private logger: LogService, 
@@ -40,50 +40,18 @@ export class PaperwalletComponent implements OnInit {
     ngOnInit() {
     }
 
-    //the method we are using currently
     print(): void {
 
-        const electron = require('electron');
-        const BrowserWindow = electron.remote.BrowserWindow;
+        const BrowserWindow = this.electronService.remote.BrowserWindow;;
 
-        let printContents;
-        printContents = this.winRef.nativeWindow.document.getElementById('printsection').innerHTML;
-        /*this.winRef.nativeWindow.document.open();
-        this.winRef.nativeWindow.document.write(`
-          <html>
-            <head>
-              <title>Print tab</title>
-              <style>
-              //........Customized style.......
-              </style>
-            </head>
-            <body align="center"><h1>Paper Wallet</h1>${printContents}
-                <button type="button" onclick="redirect()">GO Back</button>
-                <nav>
-                <a routerLink="/paperwallet" href="#"> Anchor </a>
-                </nav>
-                <nav>
-                <button routerLink="/paperwallet"> Anchor </button>
-                </nav>
-                <button routerLink="/paperwallet"> Anchor </button>
-                <script>
-                    function redirect() {
-                        console.log(document.location.href);
-                        window.location = document.location.href;
-                    }
-                </script>
-            </body>
-          </html>`
-        );
-        this.winRef.nativeWindow.print();    
-        this.winRef.nativeWindow.document.close();  
-        this.winRef.nativeWindow.location = __dirname + '/index.html#/home/paperwallet';
-        this.router.navigate(['home','transactions']); */
+        let printContents = this.winRef.nativeWindow.document.getElementById('printsection').innerHTML;
 
-        let win = new BrowserWindow({width: 800, height: 700,icon: __dirname + '/favicon.ico'})
+        let win = new BrowserWindow({width: 800, height: 700,icon: __dirname + '/favicon.ico'});
+
         win.on('closed', () => {
           win = null;
-        })
+        });
+        
         const loadView = ({title,scriptUrl}) => {
           return (`
           <html>
@@ -121,25 +89,44 @@ export class PaperwalletComponent implements OnInit {
             <p> A CasinoCoin Paper Wallet is an offline mechanism for storing CasinoCoin. The process involves printing the public and private keys onto paper.</p>
             <strong>Disclaimer:</strong> xxxxxxxxx xxxxxxxxxx xxxxxxxxxx xxxxxxxxxx
             ${printContents}
+            <div>
+                <h2>Instructions</h2>
+                <b>Step 0. Follow the security checklist recommendation</b><br>
+                <p>First step is to download this web page from Github and open the index.html file directly from your computer. It's just too easy to sneak some evil code in the 6000+ lines of javascript to leak your private key, and you don't want to see your fund stolen. Code version control make it much easier to cross-check what actually run. For extra security, unplug your Internet access while generating your wallet.<br>
+                </p><br>
+                <b>Step 1. Generate New Address</b><br>
+                <p>Click on the "Generate New Address" button.<br>
+                </p><br>
+                <b>Step 2. Print the Paper Wallet</b><br>
+                <p>Print the page on high quality setting. Never save the page as a PDF file to print it later since a file is more likely to be hacked than a piece of paper.<br>
+                </p><br>
+                <b>Step 3. Share your public address</b><br>
+                <p>Use your public address to receive CasinoCoin from other users. You can share your public address as much as you want.<br>
+                </p><br>
+                <b>Step 4. Keep your private key secret</b><br>
+                <p>The private key is literally the keys to your coins, if someone was to obtain it, they could withdraw the funds currently in the wallet, and any funds that might be deposited in that wallet.<br>
+                </p><br>
+                <p>Please test sending a small amount before receiving any large payments.<br></p> 
+            </div>
             </body>
           </html>`)
         }
-        var file = 'data:text/html;charset=UTF-8,' + encodeURIComponent(loadView({
+        let file = 'data:text/html;charset=UTF-8,' + encodeURIComponent(loadView({
           title: "PaperWallet",
           scriptUrl: "./paperWallet.view.js"
         }));
         win.setMenu(null);
         win.loadURL(file);
-        // setTimeout( ()=>{
-        //     win.close();
-        // }, 1000);
-        //win.webContents.print();
+    }
+
+    instructions() {
+        this.showInstructions = true;
     }
 
     showCreateAddress(){
-        var seed = keypairs.generateSeed();
-        var keypair = keypairs.deriveKeypair(seed);
-        var address = keypairs.deriveAddress(keypair.publicKey);
+        let seed = keypairs.generateSeed();
+        let keypair = keypairs.deriveKeypair(seed);
+        let address = keypairs.deriveAddress(keypair.publicKey);
 
         //var secret = casinocoin.Seed.from_bits(casinocoin.sjcl.random.randomWords(4));
         //var address = secret.get_key().get_address().to_json();
@@ -148,35 +135,6 @@ export class PaperwalletComponent implements OnInit {
         this.newSecretKey = seed;
         document.getElementById("qrcode_address").style.display = "block";
         document.getElementById("qrcode_secret").style.display = "block";
-        //this.generateQRCode();
-
-        if(this.checkBox){
-        this.accounts = this.walletService.getAllAccounts();
-            this.keySet.push({
-                account: this.accounts[0].accountID,
-                public_key: address,
-                private_key: seed
-            })
-        }
     }
 
-    generateQRCode(){
-        document.getElementById("qrcodeAddress").innerHTML = "";
-        document.getElementById("qrcodeSecret").innerHTML = "";
-        
-        var qrcodeAddress = new QRCode(
-                                document.getElementById("qrcodeAddress"), {
-                                    width : 100,
-                                    height : 100
-                            });
-
-        var qrcodeSecret = new QRCode(
-                                document.getElementById("qrcodeSecret"), {
-                                    width : 100,
-                                    height : 100
-                            });
-
-        qrcodeAddress.makeCode(this.newAddress);
-        qrcodeSecret.makeCode(this.newSecretKey);
-    }
 }
